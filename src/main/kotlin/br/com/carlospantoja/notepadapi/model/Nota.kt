1eaf64ad90924bb76d15a58ba1210b6973978ef6
CarlosPantoja <carlosrpcaldas@hotmail.com>
2018-02-26 22:21:05 -0300

    Mudou Porta Aceitou Nulos em ID

--- src/main/kotlin/br/com/carlospantoja/notepadapi/model/Nota.kt
+++ src/main/kotlin/br/com/carlospantoja/notepadapi/model/Nota.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Nota(@Id var id: String, var titulo: String, var conteudo: String)
+data class Nota(@Id var id: String? = null, var titulo: String, var conteudo: String)
